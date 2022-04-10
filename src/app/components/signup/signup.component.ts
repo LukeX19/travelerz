d6e2e4a97ed91d882662a147992d5170662af704
Cătălin Secoșan <secosan.catalinluca@gmail.com>
2022-04-10 18:14:00 +0300

    Created Sign Up Component
    Added a new page for sign up

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators} from "@angular/forms";
+
+export function passwordsMatchValidator(): ValidatorFn{
+  return (control: AbstractControl): ValidationErrors | null => {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+    if(password && confirmPassword && password != confirmPassword){
+      return{
+        passwordsDontMatch: true
+      }
+    }
+     return null;
+  };
+}
 
 @Component({
   selector: 'signup',
@@ -7,9 +21,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  signupForm = new FormGroup({
+    firstname: new FormControl('', Validators.required),
+    lastname: new FormControl('', Validators.required),
+    username: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', Validators.required),
+    confirmPassword: new FormControl('', Validators.required),
+  }, {validators: passwordsMatchValidator() })
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  get firstname(){
+    return this.signupForm.get('firstname');
+  }
+  get lastname(){
+    return this.signupForm.get('lastname');
+  }
+  get username(){
+    return this.signupForm.get('username');
+  }
+  get email(){
+    return this.signupForm.get('email');
+  }
+  get password(){
+    return this.signupForm.get('password');
+  }
+  get confirmPassword(){
+    return this.signupForm.get('confirmPassword');
   }
 
 }
