385bfaa5adfd781ae678c730aec654a8ef146772
Casian-Gabriel Șuli <ksi0711@yahoo.com>
2022-05-28 15:11:29 +0300

    Created Homepage component
    Basic search bar on the homepage
    Change the app title to "TravelerZ"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,23 @@ import {AppRoutingModule} from "./app-routing.module";
 import { SignupComponent } from './components/signup/signup.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactUsComponent } from './components/contactus/contactus.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
+import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
+import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
+import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
+import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
+import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
+import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
+import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
+import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
 
 @NgModule({
   declarations: [
@@ -25,7 +42,8 @@ import { ContactUsComponent } from './components/contactus/contactus.component';
     LoginComponent,
     SignupComponent,
     AboutComponent,
-    ContactUsComponent
+    ContactUsComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
@@ -39,6 +57,22 @@ import { ContactUsComponent } from './components/contactus/contactus.component';
     ReactiveFormsModule,
     MatMenuModule,
     RouterModule,
+    MatDatepickerModule,
+    MdbAccordionModule,
+    MdbCarouselModule,
+    MdbCheckboxModule,
+    MdbCollapseModule,
+    MdbDropdownModule,
+    MdbFormsModule,
+    MdbModalModule,
+    MdbPopoverModule,
+    MdbRadioModule,
+    MdbRangeModule,
+    MdbRippleModule,
+    MdbScrollspyModule,
+    MdbTabsModule,
+    MdbTooltipModule,
+    MdbValidationModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
