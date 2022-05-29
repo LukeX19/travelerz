4f7089a768e4741c6c3cbd37b513ba3c575d2e14
Casian-Gabriel Șuli <ksi0711@yahoo.com>
2022-05-29 18:05:07 +0300

    Created Hotel_List-Item component
    Created a template card for the items that will be displayed after the search button is pressed

---
+++ src/app/components/hotellist-item/hotellist-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hotellist-item',
+  templateUrl: './hotellist-item.component.html',
+  styleUrls: ['./hotellist-item.component.css']
+})
+export class HotellistItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
